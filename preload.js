4c357c91beb796bf516a61d4fb612ea0f2b3c624
NeeharPrem <neeharprem94@gmail.com>
2024-12-04 06:49:09 +0530

    feat: ipc handlers added to preload

--- preload.js
+++ preload.js
@@ -2,6 +2,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+    onPaymentMethod: (callback) => {
+        ipcRenderer.on('payment-method', (event, method) => callback(method));
+    },
+    onSearchFocus: (callback) => {
+        ipcRenderer.on('focus-search', () => callback());
+    }
 });
 window.addEventListener('DOMContentLoaded', () => {
 });
